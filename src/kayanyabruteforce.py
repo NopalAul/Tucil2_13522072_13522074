83b869c34bd4fe972a02b2e37008711ddc66c1b0
NopalAul <mnaufal0605@gmail.com>
2024-03-17 15:39:25 +0700

    Update kayanyabruteforce.py

--- src/kayanyabruteforce.py
+++ src/kayanyabruteforce.py
@@ -1,38 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from time import process_time
 
-def de_casteljau(points, t):
+#################### ALGORITMA ####################
+def bezier_linear(points, t):
     # Basis case: jika hanya satu titik tersisa, kembalikan titik tersebut
     if len(points) == 1:
         return points[0]
 
-    # Lakukan interpolasi linear antara semua pasangan titik berurutan
+    # Lakukan interpolasi linear antara semua pasangan titik berurutan (kurva Bézier linier)
     new_points = []
     for i in range(len(points) - 1):
         new_points.append((1 - t) * points[i] + t * points[i + 1])
 
-    # Rekursi pada submasalah dengan titik-titik baru
-    return de_casteljau(new_points, t)
+    # Rekursi dengan titik-titik baru
+    return bezier_linear(new_points, t)
 
-def bezier_curve_divide_and_conquer(points, iterations):
+def bezier_curve_brute_force(points, iterations):
+    # Kalkulasi titik dari iterasi yang diberikan
+    # Dari ekivalensi parameter iteration pada Divide and Conquer
+    n_point = (2 ** iterations) + 1
+
+    print("ALGORITMA BRUTE FORCE")
+    print(f'Jumlah iterasi  : {iterations}')
+    print(f'Jumlah titik    : {n_point}')
+
     # Array untuk menyimpan titik-titik pada kurva Bezier
     curve_points = []
 
-    # Iterasi melalui berbagai nilai parameter t
-    for t in np.linspace(0, 1, iterations):
-        # Hitung titik pada kurva menggunakan algoritma divide and conquer
-        curve_point = de_casteljau(points, t)
+    # Iterasi parameter t sesuai jumlah titik
+    for t in np.linspace(0, 1, n_point):
+        curve_point = bezier_linear(points, t)
         curve_points.append(curve_point)
 
     return curve_points
 
-# Contoh penggunaan
-points = np.array([[0, 0], [2, 5], [5, 3], [7, 8], [10, 15]])  # Contoh dengan 5 titik
-iterations = 10  # Jumlah iterasi
+# Penggunaan
+points = np.array([[0, 0], [1.5, 4], [4, 0], [5, 3]])
+iterations = 11
 
+# Start time
+start = process_time()
 # Membuat kurva Bezier
-curve_points = bezier_curve_divide_and_conquer(points, iterations)
+curve_points = bezier_curve_brute_force(points, iterations)
+# End time
+end = process_time()
+timer = round(end - start, 2) * 1000
+print(f'Waktu eksekusi  : {timer} ms')
 
+
+#################### PLOT KURVA ####################
 # Memisahkan koordinat x dan y
 x = [point[0] for point in curve_points]
 y = [point[1] for point in curve_points]
@@ -43,7 +60,7 @@ plt.plot(x, y, marker='o', linestyle='-')
 # Plot titik kontrol
 plt.scatter([point[0] for point in points], [point[1] for point in points], color='red')
 
-# Menambahkan label
+# Label nama
 plt.title('Bezier Curve')
 plt.xlabel('X')
 plt.ylabel('Y')
